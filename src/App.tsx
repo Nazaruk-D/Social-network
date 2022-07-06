ae0eceab162a2241c932cd78fc5f75dd5d7c62f7
Nazaruk-D <nazaruk-dima@mail.ru>
2022-07-06 12:19:17 +0300

    Route -> render

--- src/App.tsx
+++ src/App.tsx
@@ -13,22 +13,22 @@ import {Settings} from "./components/Settings/Settings";
 const App = () => {
     return (
         <BrowserRouter>
-        <div className="app-wrapper">
-            <Header
-                img={"https://www.eurohandball.com/media/f45ofcxo/ehf_og_image2.jpg?anchor=center&mode=crop&width=1200&height=630&rnd=132375557452270000"}/>
-            <Nav profile={"Profile"}
-                 message={"Message"}
-                 news={"News"}
-                 music={"Music"}
-                 settings={"Settings"}/>
-            <div className={"app-wrapper-content"}>
-                <Route path="/dialogs" component={Dialogs}/>
-                <Route path="/profile" component={Profile}/>
-                <Route path="/news" component={News}/>
-                <Route path="/music" component={Music}/>
-                <Route path="/settings" component={Settings}/>
+            <div className="app-wrapper">
+                <Header
+                    img={"https://www.eurohandball.com/media/f45ofcxo/ehf_og_image2.jpg?anchor=center&mode=crop&width=1200&height=630&rnd=132375557452270000"}/>
+                <Nav profile={"Profile"}
+                     message={"Message"}
+                     news={"News"}
+                     music={"Music"}
+                     settings={"Settings"}/>
+                <div className={"app-wrapper-content"}>
+                    <Route path="/dialogs" render={() => <Dialogs/>}/>
+                    <Route path="/profile" render={() => <Profile/>}/>
+                    <Route path="/news" render={() => <News/>}/>
+                    <Route path="/music" render={() => <Music/>}/>
+                    <Route path="/settings" render={() => <Settings/>}/>
+                </div>
             </div>
-        </div>
         </BrowserRouter>
     );
 }
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,9 @@ import {MyPosts} from "./Myposts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 
 
+
 export const Profile = () => {
+
     return (<div className={s.profile}>
         <ProfileInfo img={"https://www.eurohandball.com/media/f0rhclep/microsoftteams-image-267.png?center=0.40662327708788731,0.27796471311115661&mode=crop&width=1980&height=768&rnd=132863759739570000"}/>
         <MyPosts/>
