b9c14b8d070cfa91e54dca0aa907818dc0a90220
Nazaruk-D <nazaruk-dima@mail.ru>
2022-08-19 19:53:50 +0300

    L52 side Effect

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -33,14 +33,18 @@ export type UsersPropsType = {
 
 export const Users: React.FC<UsersPropsType> = (props: UsersPropsType) => {
 
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
-        });
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            });
+        }
     }
+
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
