f0fc01a9d18c61fb3a1f207c63291b05653deed8
Nazaruk-D <nazaruk-dima@mail.ru>
2022-10-09 13:08:22 +0300

    84 - hook, useState

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+
+type ProfileStatusType = {
+    status: string
+    updateStatus: (status: string) => void
+}
+
+const ProfileStatusWithHooks: React.FC<ProfileStatusType> = (props) => {
+
+    const [editMode, setEditMode] = useState(false)
+    const [status, setStatus] = useState(props.status)
+
+    const deactivateEditMode= (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStatus(e.currentTarget.value)
+        props.updateStatus(status)
+    }
+
+    return (
+        <div>
+            {!editMode &&
+                <div>
+                        <span
+                            onDoubleClick={() => setEditMode(true)}>{props.status ? props.status : "изменить статус"}</span>
+                </div>
+            }
+            {editMode &&
+                <div>
+                    <input
+                        onChange={deactivateEditMode}
+                        value={status}
+                        onBlur={() => setEditMode(false)}
+                        autoFocus>
+                    </input>
+                </div>
+            }
+        </div>
+    );
+}
+
+export default ProfileStatusWithHooks;
